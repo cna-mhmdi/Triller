884d2ff07546c2f2abc3accb7b19c74234b434c3
cna <cna.mhmdi0435@gmail.com>
2023-08-25 21:01:08 +0330

    welcomeActivity coroutine intent & setup and

---
+++ app/src/main/java/com/cna/parde/WelcomeActivity.kt
@@ -0,0 +1,24 @@
+package com.cna.parde
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import com.cna.parde.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnWelcome.setOnClickListener {
+            val intent = Intent(this,InfoActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
