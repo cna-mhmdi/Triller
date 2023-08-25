694eb8a99977e1babd26832cf9534729adf804b2
cna <cna.mhmdi0435@gmail.com>
2023-08-25 20:32:53 +0330

    activityIntro Added

---
+++ app/src/main/java/com/cna/parde/IntroActivity.kt
@@ -0,0 +1,36 @@
+package com.cna.parde
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import com.cna.parde.databinding.ActivityIntroBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val fadeIn = AlphaAnimation(0f, 1f)
+        fadeIn.duration = 700
+        fadeIn.fillAfter = true
+        binding.introTextView.startAnimation(fadeIn)
+
+        val intent = Intent(this,WelcomActivity::class.java)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
