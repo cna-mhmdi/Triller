336c365ba9af636897704eacf1ad7701e3cfda5d
cna <cna.mhmdi0435@gmail.com>
2023-08-25 21:25:27 +0330

    SharedPreferences for IntroActivity added

--- app/src/main/java/com/cna/parde/IntroActivity.kt
+++ app/src/main/java/com/cna/parde/IntroActivity.kt
@@ -1,6 +1,8 @@
 package com.cna.parde
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.AlphaAnimation
@@ -13,23 +15,36 @@ import kotlinx.coroutines.launch
 class IntroActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityIntroBinding
-
+    private lateinit var introPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityIntroBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        introPreferences = getSharedPreferences("introPreferences", Context.MODE_PRIVATE)
+        val isIntroShowed = introPreferences.getBoolean("introPreferences",false)
+
         val fadeIn = AlphaAnimation(0f, 1f)
         fadeIn.duration = 700
         fadeIn.fillAfter = true
         binding.introTextView.startAnimation(fadeIn)
 
-        val intent = Intent(this,WelcomeActivity::class.java)
-
         CoroutineScope(Dispatchers.Main).launch {
             delay(1000)
-            startActivity(intent)
+
+            val targetActivity = if (!isIntroShowed) {
+
+                val editor = introPreferences.edit()
+                editor.putBoolean("introPreferences",true)
+                editor.apply()
+                WelcomeActivity::class.java
+
+            } else {
+                MainActivity::class.java
+            }
+            startActivity(Intent(this@IntroActivity,targetActivity))
+
             finish()
         }
     }
