53fa69469e77c95d2fa1c9773f515fecca2f1601
cna <cna.mhmdi0435@gmail.com>
2023-08-27 00:30:14 +0330

    chips and name intents added

--- app/src/main/java/com/cna/parde/InfoActivity.kt
+++ app/src/main/java/com/cna/parde/InfoActivity.kt
@@ -1,11 +1,50 @@
 package com.cna.parde
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.children
+import com.cna.parde.databinding.ActivityInfoBinding
+import com.google.android.material.chip.Chip
+import com.google.android.material.snackbar.Snackbar
 
 class InfoActivity : AppCompatActivity() {
+    companion object {
+        const val TAG_USERNAME = "USERNAME"
+        const val TAG_CHIP_NAMES = "CHIP_NAMES"
+    }
+
+    private lateinit var binding : ActivityInfoBinding
+    private val selectedChipNames = mutableListOf<String>()
+    private lateinit var userNames :String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_info)
+        binding = ActivityInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnInfo.setOnClickListener {
+
+            if(!binding.txtName.text.isNullOrEmpty()) {
+
+                userNames = binding.txtName.text.toString()
+
+                selectedChipNames.clear()
+                binding.chipGroup.children.toList().filterIsInstance<Chip>()
+                    .filter { it.isChecked }
+                    .forEach { chip ->
+                        val chipName = chip.text.toString()
+                        selectedChipNames.add(chipName)
+                    }
+                selectedChipNames.joinToString(", ")
+                val intent = Intent(this,MainActivity::class.java)
+                intent.putExtra(TAG_USERNAME, userNames)
+                intent.putStringArrayListExtra(TAG_CHIP_NAMES,ArrayList(selectedChipNames))
+                startActivity(intent)
+            }else{
+                Snackbar.make(binding.root,"لطفا نام خود را وارد کنید !",Snackbar.LENGTH_LONG)
+                    .setBackgroundTint(resources.getColor(R.color.main_background))
+                    .show()
+            }
+        }
     }
 }
--- app/src/main/java/com/cna/parde/MainActivity.kt
+++ app/src/main/java/com/cna/parde/MainActivity.kt
@@ -2,10 +2,26 @@ package com.cna.parde
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG_USERNAME = "USERNAME"
+        const val TAG_CHIP_NAMES = "CHIP_NAMES"
+    }
+
+    private val userName: String by lazy {
+        intent.getStringExtra(TAG_USERNAME) ?:"مهمان"
+    }
+
+    //remember if list was empty
+    private val selectedChipNames: List<String> by lazy {
+        intent.getStringArrayListExtra(TAG_CHIP_NAMES) ?: emptyList()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"$userName$selectedChipNames",Toast.LENGTH_LONG).show()
     }
 }
