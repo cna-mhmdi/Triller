84d3cd9b4e2e15268865caf7ea335185d88380d4
cna <cna.mhmdi0435@gmail.com>
2023-08-29 19:57:34 +0330

    bottom navigation req added

---
+++ app/src/main/java/com/cna/parde/fragments/HomeFragment.kt
@@ -0,0 +1,59 @@
+package com.cna.parde.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cna.parde.adapters.ViewPagerAdapter
+import com.cna.parde.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayout
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    companion object {
+        const val TAG_USERNAME = "USERNAME"
+        const val TAG_CHIP_NAMES = "CHIP_NAMES"
+    }
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Movie"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Tv"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Anime"))
+        binding.tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter =
+            ViewPagerAdapter(requireActivity(),childFragmentManager,binding.tabLayout.tabCount)
+        binding.viewPager.adapter = adapter
+
+        binding.viewPager
+            .addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                binding.viewPager.currentItem = tab!!.position
+
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?){}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
