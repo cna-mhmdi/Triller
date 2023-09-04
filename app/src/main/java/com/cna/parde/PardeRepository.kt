4b4e8bda490a5ff04603d9b8b2084d8f6698f17b
cna <cna.mhmdi0435@gmail.com>
2023-09-04 22:02:39 +0330

    repository added

--- app/src/main/java/com/cna/parde/PardeRepository.kt
+++ app/src/main/java/com/cna/parde/PardeRepository.kt
@@ -2,28 +2,39 @@ package com.cna.parde
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.cna.parde.api.PopularMovieService
-import com.cna.parde.model.Movie
+import com.cna.parde.api.MovieService
+import com.cna.parde.model.PopularMovie
+import com.cna.parde.model.UCMovie
 
-class PardeRepository(private val popularMovieService: PopularMovieService) {
+class PardeRepository(private val movieService: MovieService) {
 
     private val apiKey = "721e4f18664869b21649e0be3e99ec59"
 
-    private val movieLiveData = MutableLiveData<List<Movie>>()
-    private val errorLiveData = MutableLiveData<String>()
+    private val popularMovieLiveData = MutableLiveData<List<PopularMovie>>()
+    private val popularMovieErrorLiveData = MutableLiveData<String>()
 
-    val movies: LiveData<List<Movie>>
-        get() = movieLiveData
+    val popularMovies: LiveData<List<PopularMovie>>
+        get() = popularMovieLiveData
 
-    val error: LiveData<String>
-        get() = errorLiveData
+    val popularMovieError: LiveData<String>
+        get() = popularMovieErrorLiveData
+
+    private val upcomingMovieLiveData = MutableLiveData<List<UCMovie>>()
+    private val upcomingMovieErrorLiveData = MutableLiveData<String>()
+
+    val upcomingMovies: LiveData<List<UCMovie>>
+        get() = upcomingMovieLiveData
+    val upcomingMovieError: LiveData<String>
+        get() = upcomingMovieErrorLiveData
 
     suspend fun fetchMovies() {
         try {
-            val popularMovies = popularMovieService.getPopularMovie(apiKey)
-            movieLiveData.postValue(popularMovies.results)
+            val popularMovies = movieService.getPopularMovie(apiKey)
+            val upcomingMovies = movieService.getUpcomingMovie(apiKey)
+            popularMovieLiveData.postValue(popularMovies.results)
+            upcomingMovieLiveData.postValue(upcomingMovies.results)
         } catch (exception: Exception) {
-            errorLiveData.postValue("An error occurred: ${exception.message}")
+            popularMovieErrorLiveData.postValue("An error occurred: ${exception.message}")
         }
     }
 }
