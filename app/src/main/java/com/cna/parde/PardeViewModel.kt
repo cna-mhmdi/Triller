a9ba4a0b691a11f8a2bf7449ce516d2bf9032537
cna <cna.mhmdi0435@gmail.com>
2023-09-04 22:03:02 +0330

    view Model added

--- app/src/main/java/com/cna/parde/PardeViewModel.kt
+++ app/src/main/java/com/cna/parde/PardeViewModel.kt
@@ -3,22 +3,26 @@ package com.cna.parde
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.cna.parde.model.Movie
+import com.cna.parde.model.PopularMovie
+import com.cna.parde.model.UCMovie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class PardeViewModel(private val pardeRepository: PardeRepository): ViewModel() {
 
     init {
-        fetchPopularMovies()
+        fetchMovies()
     }
 
-    val popularMovies: LiveData<List<Movie>>
-        get() = pardeRepository.movies
+    val popularMovies: LiveData<List<PopularMovie>>
+        get() = pardeRepository.popularMovies
 
-    fun getError(): LiveData<String> = pardeRepository.error
+    fun getPopularMovieError(): LiveData<String> = pardeRepository.popularMovieError
 
-    private fun fetchPopularMovies() {
+    val upComingMovie: LiveData<List<UCMovie>> get() = pardeRepository.upcomingMovies
+    fun getUpComingMovieError(): LiveData<String> = pardeRepository.upcomingMovieError
+
+    private fun fetchMovies() {
         viewModelScope.launch(Dispatchers.IO) {
             pardeRepository.fetchMovies()
         }
