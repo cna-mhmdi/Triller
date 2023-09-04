7c4c4c765db382aeb47ca0e3c898be0d2d3df30d
cna <cna.mhmdi0435@gmail.com>
2023-09-04 22:03:21 +0330

    uc adapter added

--- app/src/main/java/com/cna/parde/adapters/UCMovieAdapter.kt
+++ app/src/main/java/com/cna/parde/adapters/UCMovieAdapter.kt
@@ -3,12 +3,18 @@ package com.cna.parde.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.bumptech.glide.Glide
 import com.cna.parde.R
+import com.cna.parde.model.UCMovie
 
-class UCMovieAdapter(private var names: Array<String>,private val rates : Array<String>)
+class UCMovieAdapter(private val clickListener: UCMovieClickListener)
     : RecyclerView.Adapter<UCMovieAdapter.UCMovieViewHolder>() {
+
+    private val movies = mutableListOf<UCMovie>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UCMovieViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -17,27 +23,41 @@ class UCMovieAdapter(private var names: Array<String>,private val rates : Array<
     }
 
     override fun onBindViewHolder(holder: UCMovieViewHolder, position: Int) {
-        val btnNames = names[position]
-        val btnRates = rates[position]
-        holder.bind(btnNames,btnRates)
+        val movie = movies[position]
+        holder.bind(movie)
+        holder.itemView.setOnClickListener { clickListener.onUCMovieClick(movie) }
     }
 
     override fun getItemCount(): Int {
-        return names.size
+        return movies.size
     }
 
-    fun updateData(newNames: Array<String>){
-        names = newNames
-        notifyDataSetChanged()
+    fun addMovies(movieList: List<UCMovie>){
+        movies.addAll(movieList)
+        notifyItemRangeInserted(0,movieList.size)
     }
 
     inner class UCMovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        private val txtTitle: TextView = itemView.findViewById(R.id.txtTitleMovie)
-        private val txtRate: TextView = itemView.findViewById(R.id.txtRateMovie)
+        private val txtTitle: TextView by lazy {itemView.findViewById(R.id.txtTitleMovie)}
+        private val txtRate: TextView by lazy {itemView.findViewById(R.id.txtRateMovie)}
+        private val imgMoviePic: ImageView by lazy { itemView.findViewById(R.id.imgMoviePic) }
+        private val imageUrl = "https://image.tmdb.org/t/p/w185/"
 
-        fun bind(names: String,rates: String){
-            txtTitle.text = names
-            txtRate.text = rates
+        fun bind(movie: UCMovie){
+            txtTitle.text = movie.title
+            txtRate.text = movie.vote_average.toString()
+
+            imgMoviePic.load("$imageUrl${movie.poster_path}")
+
+//            Glide.with(itemView.context)
+//                .load("$imageUrl${movie.poster_path}")
+//                .placeholder(R.drawable.star)
+//                .fitCenter()
+//                .into(imgMoviePic)
         }
+    }
+
+    interface UCMovieClickListener {
+        fun onUCMovieClick(movie: UCMovie)
     }
 }
